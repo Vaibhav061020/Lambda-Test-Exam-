62c02c91466f0a62b33779c66013e9f1347a13a4
vaibhav061020 <vaibhavpawar061020@gmail.com>
2022-08-25 22:26:15 +0530

    Test scenario2 updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lambda-Test-Exam-
 This repo is created for the lambda test exam prespective
+
---
+++ cypress/support/index.js
@@ -0,0 +1,7 @@
+// Import commands.js using ES2015 syntax:
+import './commands'
+import 'cypress-axe'
+import 'cypress-audit/commands';
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+require('cypress-xpath');
